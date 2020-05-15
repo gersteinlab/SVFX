0668aa3f0643cd088ca0dcd0fbeeaca00afe9fed
jagathv <jagathvythee@gmail.com>
2020-05-15 17:09:03 -0500

    minor changes

--- gfm_script.py
+++ gfm_script.py
@@ -12,6 +12,6 @@ bigwig_files = "-b " + " ".join(snakemake.config['matrix_params']['bigwig_files'
 os.system(f"python3 src/generate_feature_matrix.py -c {control} {bigwig_files} {coord_files} -o {control_matrix} -t {variant_type} {normalized} -r {randomized_num} -rg {ref_genome}")
 os.system(f"python3 src/generate_feature_matrix.py -c {disease} {bigwig_files} {coord_files} -o {disease_matrix} -t {variant_type} {normalized} -r {randomized_num} -rg {ref_genome} -f")
 combined_matrix = f"data/{snakemake.config['run_id']}_combined_matrix.txt"
-control_matrix = f"{snakemake.config['output_matrix']['control']}_{snakemake.config['run_id']}_normalized.txt" if snakemake.config['matrix_params']['normalized'] else "{snakemake.config['output_matrix']['control']}_{snakemake.config['run_id']}.txt"
-disease_matrix = f"{snakemake.config['output_matrix']['disease']}_{snakemake.config['run_id']}_normalized.txt" if snakemake.config['matrix_params']['normalized'] else "{snakemake.config['output_matrix']['disease']}_{snakemake.config['run_id']}.txt"
+control_matrix = f"{snakemake.config['output_matrix']['control']}_{snakemake.config['run_id']}_normalized.txt" if snakemake.config['matrix_params']['normalized'] else f"{snakemake.config['output_matrix']['control']}_{snakemake.config['run_id']}.txt"
+disease_matrix = f"{snakemake.config['output_matrix']['disease']}_{snakemake.config['run_id']}_normalized.txt" if snakemake.config['matrix_params']['normalized'] else f"{snakemake.config['output_matrix']['disease']}_{snakemake.config['run_id']}.txt"
 os.system(f"cat {disease_matrix} > {combined_matrix} && tail -n +2 {control_matrix} >> {combined_matrix}")
